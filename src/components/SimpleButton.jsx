c7811a814b3abba617fbdf832b895a08668e4b89
StepanGrigorchuk <grigorchuksv13@gmail.com>
2025-06-03 00:51:17 +0300

    Дополнения по типографике, размер текста тегов и кнопок

--- src/components/SimpleButton.jsx
+++ src/components/SimpleButton.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 function SimpleButton({ to, href, onClick, children, variant = "primary", className = "", ...props }) {
   const variants = {
-    primary: "px-4 py-2 bg-[var(--primary)] text-[var(--white)] rounded-lg hover:bg-[var(--blue-600)] transition text-button",
-    secondary: "px-4 py-2 bg-[var(--gray-200)] text-[var(--gray-600)] rounded-lg hover:bg-[var(--gray-300)] transition text-button",
-    outline: "px-4 py-2 border border-[var(--primary)] text-[var(--primary)] rounded-lg hover:bg-[var(--primary)] hover:text-[var(--white)] transition text-button"
+    primary: "px-4 py-4 bg-[var(--primary)] text-[var(--white)] rounded-lg hover:bg-[var(--blue-600)] transition text-button",
+    secondary: "px-4 py-4 bg-[var(--gray-200)] text-[var(--gray-600)] rounded-lg hover:bg-[var(--gray-300)] transition text-button font-normal",
+    outline: "px-4 py-4 border border-[var(--primary)] text-[var(--primary)] rounded-lg hover:bg-[var(--primary)] hover:text-[var(--white)] transition text-button"
   };
   
   const baseClasses = variants[variant] || variants.primary;
