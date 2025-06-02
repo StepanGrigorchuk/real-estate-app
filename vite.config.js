e059be769f982ac18d610dd365ff6e8e0e6f51bd
StepanGrigorchuk <grigorchuksv13@gmail.com>
2025-06-02 23:13:20 +0300

    Подключение ngrok

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+  plugins: [react()],  resolve: {
     extensions: ['.js', '.jsx'], // Автоматически разрешаем .jsx
   },
   server: {
+    host: true, // Разрешает доступ с внешних хостов
+    port: 5173,
+    allowedHosts: [
+      '3f3b-45-85-105-94.ngrok-free.app',
+      '.ngrok-free.app', // Разрешаем все ngrok домены
+      'localhost',
+      '127.0.0.1'
+    ],
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://localhost:3000', 
     },
   },
 });
