f5f09449637fb39e89f66b6a8d37d07fabd18341
StepanGrigorchuk <grigorchuksv13@gmail.com>
2025-06-02 18:54:15 +0300

    Фикс хедера на мобилках

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,32 +31,32 @@ function Header() {
 
   return (
     <header
-      className={`bg-white shadow-md py-2 sm:py-4 px-2 sm:px-6 fixed w-full z-10 transition-transform duration-500`}
+      className={`bg-white shadow-md py-4 sm:py-5 px-4 sm:px-6 fixed w-full z-10 transition-transform duration-500`}
       style={{
         transform: isHeaderVisible ? 'translateY(0)' : 'translateY(-100%)',
         transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
       }}
     >
-      <div className="max-w-7xl mx-auto flex flex-row flex-wrap justify-between items-center gap-2 sm:gap-0">
+      <div className="max-w-7xl mx-auto flex flex-row flex-wrap justify-between items-center gap-3 sm:gap-0">
         <Link to="/" reloadDocument className="flex items-center min-w-[40px]">
-          <img src="/logo.svg" alt="Логотип Компас" className="h-8 sm:h-10 w-auto" />
+          <img src="/logo.svg" alt="Логотип Компас" className="h-10 sm:h-10 w-auto" />
         </Link>
-        <div className="flex gap-2 sm:gap-3 items-center">
+        <div className="flex gap-3 sm:gap-3 items-center">
           <a
             href={TELEGRAM_LINK}
-            className="hidden md:inline-block bg-primary text-white px-3 sm:px-4 py-2 rounded-lg hover:bg-blue-700 transition whitespace-nowrap text-xs sm:text-base"
-            style={{ fontSize: 'var(--font-size-sm)' }}
+            className="hidden md:inline-block bg-primary text-white px-3 sm:px-4 py-2 rounded-lg hover:bg-blue-700 transition whitespace-nowrap text-base sm:text-base"
+            style={{ fontSize: 'clamp(1rem, 2vw + 0.8rem, 1.1rem)' }}
           >
             Написать помощнику
           </a>
           <a
             href="tel:+1234567890"
-            className="bg-white p-2 rounded-lg shadow-md hover:bg-gray-100 transition flex items-center"
-            style={{ fontSize: 'var(--font-size-sm)' }}
+            className="bg-white p-3 rounded-lg shadow-md hover:bg-gray-100 transition flex items-center"
+            style={{ fontSize: 'clamp(1.1rem, 2vw + 0.8rem, 1.15rem)' }}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 sm:h-6 sm:w-6 text-primary"
+              className="h-7 w-7 sm:h-6 sm:w-6 text-primary"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
